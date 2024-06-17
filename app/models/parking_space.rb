32a826325bb913ae3502014cf21b3b602a7ac161
ren <raenierquiambao@gmail.com>
2024-06-17 16:33:46 +0800

    Get nearest available parking space based on vehicle size and entrance

--- app/models/parking_space.rb
+++ app/models/parking_space.rb
@@ -1,4 +1,17 @@
 class ParkingSpace < ApplicationRecord
   enum size: %i[small medium large]
   serialize :entrance_distances, type: Array, coder: JSON
+  has_many :tickets
+
+  scope :active_spaces, -> { joins(:tickets).where(tickets: { status: 'parked' }).distinct }
+  scope :available_spaces_by_size, ->(size) { where(size:).where.not(id: active_spaces.ids) }
+
+  def self.get_nearest_available(entrance_number, size)
+    (sizes[size]..sizes.values.last).each do |size_val|
+      available_spaces = available_spaces_by_size(size_val)
+      next unless available_spaces
+
+      return available_spaces.min_by { |space| space.entrance_distances[entrance_number - 1] }
+    end
+  end
 end
