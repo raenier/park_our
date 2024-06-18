0b2317d79e53f9a196506d780f27396092d50583
ren <raenierquiambao@gmail.com>
2024-06-18 11:23:54 +0800

    Calculate fee of ticket
    Check if ticket duration is greater than 3 hrs
    if it is add continous rate to flatrate
    if its not just return initial flatrate

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,29 @@
 class Ticket < ApplicationRecord
+  FLAT_RATE = 40
+
   belongs_to :vehicle
   belongs_to :parking_space
 
   enum status: { parked: 1, unparked: 0 }
+
+  def calculate_fee
+    continuous_rate = calculate_continuous_rate
+    duration = calculate_hour_duration
+
+    return FLAT_RATE if duration <= 3
+
+    excess = duration - 3
+    FLAT_RATE + (excess * continuous_rate)
+  end
+
+  private
+
+  def calculate_continuous_rate
+    20 + (40 * ParkingSpace.sizes[parking_space.size])
+  end
+
+  def calculate_hour_duration
+    duration = (Time.zone.now - created_at) / 3600
+    duration.ceil
+  end
 end
